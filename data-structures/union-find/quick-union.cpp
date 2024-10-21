88c8da26055c7dd6a2bf5f34b51b7aa52b4c0e23
Al Amru Bil Maruf <alamrubilmaruf412@gmail.com>
2024-10-21 09:07:27 +0600

    weighted union

--- data-structures/union-find/quick-union.cpp
+++ data-structures/union-find/quick-union.cpp
@@ -4,54 +4,53 @@ using namespace std;
 
 // Quick Union
 
-int root(vector<int> components, int node) {
-    while (components[node] != node) {
-        node = components[node];
+int root(vector<int> parents, int node) {
+    while (parents[node] != node) {
+        node = parents[node];
     }
     return node;
 }
 
-bool Find(vector<int> components, int u, int v) {
-    if (root(components, u) == root(components, v)) {
+bool Find(vector<int> parents, int u, int v) {
+    if (root(parents, u) == root(parents, v)) {
         return true;
     } else {
         return false;
     }
 }
 
-void Union(vector<int> &components, int u, int v) {
-    int root_u = root(components, u);
-    int root_v = root(components, v);
+void Union(vector<int> &parents, int u, int v) {
+    int root_u = root(parents, u);
+    int root_v = root(parents, v);
 
-    components[root_v] = root_u;
+    parents[root_v] = root_u;
 }
 
-// complexity: O(N*M), where N = nodes, M = edges
 int main() {
     freopen("input.txt", "r", stdin); // read input from `input.txt`
 
     int nodes, edges, u, v;
     cin >> nodes >> edges;
 
-    vector<int> components(nodes);
+    vector<int> parents(nodes);
     for (int i = 0; i < nodes; i++) {
-        components[i] = i;
+        parents[i] = i;
     }
 
     for (int i = 0; i < edges; i++) {
         cin >> u >> v;
-        Union(components, u, v);
+        Union(parents, u, v);
     }
     
     for (int i = 0; i < nodes; i++) {
-        cout << components[i] << " ";
+        cout << parents[i] << " ";
     }
     cout << "\n";
     
-    Union(components, 2, 4);
+    Union(parents, 2, 4);
     
     for (int i = 0; i < nodes; i++) {
-        cout << components[i] << " ";
+        cout << parents[i] << " ";
     }
 
     return 0;
--- data-structures/union-find/weighted-union.cpp
+++ data-structures/union-find/weighted-union.cpp
@@ -4,55 +4,67 @@ using namespace std;
 
 // Weighted Union
 
-int root(vector<int> components, int node) {
-    while (components[node] != node) {
-        components[node] = components[components[node]]; // path compression
-        node = components[node];
+int root(vector<int> parents, int node) {
+    while (parents[node] != node) {
+        parents[node] = parents[parents[node]]; // path compression
+        node = parents[node];
     }
     return node;
 }
 
-bool Find(vector<int> components, int u, int v) {
-    if (root(components, u) == root(components, v)) {
+bool Find(vector<int> parents, int u, int v) {
+    if (root(parents, u) == root(parents, v)) {
         return true;
     } else {
         return false;
     }
 }
 
-void Union(vector<int> &components, int u, int v) {
-    int root_u = root(components, u);
-    int root_v = root(components, v);
+void Union(vector<int> &parents, vector<int> &sizes, int u, int v) {
+    int root_u = root(parents, u);
+    int root_v = root(parents, v);
 
-    components[root_v] = root_u;
+    // if both nodes are already in the same set
+    if (root_u == root_v) {
+        return;
+    } 
+
+    // make smaller tree a subtree of the larger tree
+    if (sizes[root_u] < sizes[root_v]) {
+        parents[root_u] = root_v;
+        sizes[root_v] += sizes[root_u];
+    } else {
+        parents[root_v] = root_u;
+        sizes[root_u] += sizes[root_v];
+    }
 }
 
-// complexity: O(N*M), where N = nodes, M = edges
 int main() {
     freopen("input.txt", "r", stdin); // read input from `input.txt`
 
     int nodes, edges, u, v;
     cin >> nodes >> edges;
 
-    vector<int> components(nodes);
+    vector<int> parents(nodes);
+    vector<int> sizes(nodes, 1);
     for (int i = 0; i < nodes; i++) {
-        components[i] = i;
+        parents[i] = i;
     }
 
     for (int i = 0; i < edges; i++) {
         cin >> u >> v;
-        Union(components, u, v);
+        Union(parents, sizes, u, v);
     }
     
     for (int i = 0; i < nodes; i++) {
-        cout << components[i] << " ";
+        cout << parents[i] << " ";
     }
     cout << "\n";
     
-    Union(components, 2, 4);
+    Union(parents, sizes, 2, 4);
     
     for (int i = 0; i < nodes; i++) {
-        cout << components[i] << " ";
+        cout << parents[i] << " ";
     }
 
     return 0;
